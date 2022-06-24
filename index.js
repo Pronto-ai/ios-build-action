15b1d6b94e0a9ee8f38e5550884aafbc32d4a1f5
Alex Reilly <alex.reilly@pronto.ai>
2022-06-24 10:58:01 -0700

    make output path optional

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ async function run() {
     process.env.BUILD_SDK = core.getInput("build-sdk");
     process.env.BUILD_DESTINATION = core.getInput("build-destination");
     process.env.ARCHIVE_PATH = core.getInput("archive-path")
-    process.env.BUILD_PATH = core.getInput("build-path")
 
 
     // Execute build.sh
